90cdcf82a1a7903dbff7dad976d7f276d726089e
Syed Adeel Ashraf <syedadeelashraf123@gmail.com>
2023-09-20 22:21:24 +0530

    feat: my first java program

---
+++ javaTut1.java
@@ -0,0 +1,9 @@
+public class javaTut1  {
+
+    public static void main (String[] args){
+
+        // write your code here
+        System.out.println("Hello World");
+        System.out.println("My first java Program");
+    }
+}
