744c1c0defeafc3300395bf470f2fc28800d4329
Syed Adeel Ashraf <syedadeelashraf123@gmail.com>
2023-09-21 22:56:48 +0530

    feat: This is another tutorial of java

---
+++ javaTut2.java
@@ -0,0 +1,11 @@
+public class javaTut2{
+
+    public static void main(String[] args){
+        System.out.print("This is the sum of these Numbers: ");
+        int num1 = 22;
+        int num2 = 34;
+        int num3 = 78;
+        int sum = num1 + num2 + num3 ;
+        System.out.println(sum);
+    }
+}
