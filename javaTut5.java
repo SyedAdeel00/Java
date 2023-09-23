439e686f63169d1932b78a63afe9e4999fec64ae
Syed Adeel Ashraf <syedadeelashraf123@gmail.com>
2023-09-24 00:35:02 +0530

    feat: lerner how to print values using % for int,float,string

---
+++ javaTut5.java
@@ -0,0 +1,12 @@
+public class javaTut5 {
+    public static void main(String[] args){
+
+        System.out.println(" In this we will print the values of int,char,float,string");
+        int a = 7;
+        float b =  1.234f;
+        String c = "HEY THERE!";
+
+        System.out.printf("This is integer %d, This is a float %f,  This is a String %S", a,b,c);
+        
+    }
+}
